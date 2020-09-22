fbf16d7a865b6a93388edde2e06ad95f696e03e1
FlorianSahbi <florian.sahbi@gmail.com>
2020-09-22 22:56:41 +0200

    clean code

--- routes/twitch.js
+++ routes/twitch.js
@@ -27,7 +27,7 @@ router.get('/token', async (req, res, next) => {
 
 router.get('/webhook/subscribe/:userId', async (req, res, next) => {
   const data = {
-    "hub.callback": `https://quiet-badlands-04937.herokuapp.com/twitch/${req.params.userId}`,
+    "hub.callback": `https://quiet-badlands-04937.herokuapp.com/twitch/webhook/${req.params.userId}`,
     "hub.mode": "subscribe",
     "hub.topic": `https://api.twitch.tv/helix/streams?user_id=${req.params.userId}`,
     "hub.lease_seconds": 864000,
@@ -40,20 +40,9 @@ router.get('/webhook/subscribe/:userId', async (req, res, next) => {
   }
 });
 
-
-router.get("/:userId", (req, res) => {
-  console.log(req)
-  res.status(200).send(req.query[Object.keys(req.query)[0]])
-})
-
-router.post("/:userId", (req, res) => {
-  console.log(req.body)
-  res.status(200)
-})
-
 router.get('/webhook/unsubscribe/:userId', async (req, res, next) => {
   const data = {
-    "hub.callback": `https://quiet-badlands-04937.herokuapp.com/twitch/${req.params.userId}`,
+    "hub.callback": `https://quiet-badlands-04937.herokuapp.com/twitch/webhook/${req.params.userId}`,
     "hub.mode": "unsubscribe",
     "hub.topic": `https://api.twitch.tv/helix/streams?user_id=${req.params.userId}`,
     "hub.lease_seconds": 864000,
@@ -69,10 +58,21 @@ router.get('/webhook/unsubscribe/:userId', async (req, res, next) => {
 router.get('/subscriptions', async (req, res, next) => {
   try {
     const response = await axios.get("https://api.twitch.tv/helix/webhooks/subscriptions", { params: { first: 10 }, headers });
+    console.log(response)
     res.send(response.data);
   } catch (error) {
     res.send(error);
   }
 });
+
+router.get("/webhook/:userId", (req, res) => {
+  res.status(200).send(req.query[Object.keys(req.query)[0]])
+})
+
+router.post("/webhook/:userId", (req, res) => {
+  console.log(req.body)
+  // CODE DISCORD
+  res.status(200)
+})
 
 module.exports = router;

90d8c91e664abb55598a32e79be1762390b1d2e3
FlorianSahbi <florian.sahbi@gmail.com>
2020-09-22 22:13:43 +0200

    osef

--- routes/twitch.js
+++ routes/twitch.js
@@ -14,25 +14,30 @@ router.get('/token', async (req, res, next) => {
   const params = {
     client_id: TWITCH_PUBLIC,
     client_secret: TWITCH_SECRET,
-    grant_type: "client_credentials"
+    grant_type: "client_credentials",
   }
   try {
     const response = await axios.post("https://id.twitch.tv/oauth2/token", null, { params })
     res.send(response.data);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
   res.send('1');
 });
 
-router.get('/webhook/subscribe/', (req, res, next) => {
+router.get('/webhook/subscribe/', async (req, res, next) => {
   const data = {
     "hub.callback": "https://quiet-badlands-04937.herokuapp.com/twitch/confirm",
     "hub.mode": "subscribe",
     "hub.topic": "https://api.twitch.tv/helix/streams?user_id=63875647",
     "hub.lease_seconds": 864000,
   }
-  res.send('2');
+  try {
+    await axios.post("https://api.twitch.tv/helix/webhooks/hub", data, { headers });
+    res.send("OkBoomer");
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 router.get("/confirm", (req, res) => {
@@ -40,12 +45,28 @@ router.get("/confirm", (req, res) => {
   res.status(200).send(req.query[Object.keys(req.query)[0]])
 })
 
-router.get('/webhook/unsubscribe/:userId', (req, res, next) => {
-  res.send('3');
+router.get('/webhook/unsubscribe/', async (req, res, next) => {
+  const data = {
+    "hub.callback": "https://quiet-badlands-04937.herokuapp.com/twitch/confirm",
+    "hub.mode": "unsubscribe",
+    "hub.topic": "https://api.twitch.tv/helix/streams?user_id=63875647",
+    "hub.lease_seconds": 864000,
+  }
+  try {
+    await axios.post("https://api.twitch.tv/helix/webhooks/hub", data, { headers });
+    res.send("OkBoomer")
+  } catch (error) {
+    res.send(error)
+  }
 });
 
 router.get('/subscriptions', (req, res, next) => {
-  res.send('4');
+  try {
+    const response = await axios.get("https://api.twitch.tv/helix/webhooks/subscriptions", { params: { first: 10 }, headers });
+    res.send(response.data);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
